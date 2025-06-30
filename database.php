2ad068a8fa99cc7888e5cb03af2e753deccac696
JABO <jabogabiro1@gmail.com>
2025-06-30 21:57:39 +0200

    Update database.php

--- database.php
+++ database.php
@@ -5,10 +5,10 @@ $password= "";
 $database= "books";
 $connection=mysqli_connect($server, $username, $password, $database);
 if(isset($_POST["send"])){
-  $Title=$_POST["title"];
-  $Author=$_POST["author"];
-  $Year=$_POST["years"];
-  $Genre=$_POST["genre"];
+  $Title= mysqli_real_escape_string ($connection,$_POST["title"]);
+  $Author=mysqli_real_escape_string ($connection,$_POST["author"]);
+  $Year=mysqli_real_escape_string ($connection,$_POST["years"]);
+  $Genre=mysqli_real_escape_string($connection,$_POST["genre"]);
   $query="INSERT INTO big(title, author, years, genre)VALUES('$Title', '$Author', '$Year', '$Genre')";
   mysqli_query($connection, $query);
   header("location:index.php");
